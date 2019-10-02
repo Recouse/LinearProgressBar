3fdb46cec15d9e68f07948c8d5f02b61517a60a0
Антон Лисовой <lisovoy@arcsinus.ru>
2019-10-02 16:49:40 +0300

    Framework update

---
+++ Frameworks/LinearProgressBar.framework/Headers/LinearProgressBar.h
@@ -0,0 +1,19 @@
+//
+//  LinearProgressBar.h
+//  LinearProgressBar
+//
+//  Created by Firdavs Khaydarov on 09/03/18.
+//  Copyright © 2018 Firdavs Khaydarov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for LinearProgressBar.
+FOUNDATION_EXPORT double LinearProgressBarVersionNumber;
+
+//! Project version string for LinearProgressBar.
+FOUNDATION_EXPORT const unsigned char LinearProgressBarVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LinearProgressBar/PublicHeader.h>
+
+
